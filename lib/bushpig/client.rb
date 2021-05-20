756c7dc7ecea14c4e1d45be37b348e6163549b66
Shaun Sharples <shaun.sharples@gmail.com>
2021-05-20 12:06:26 +0200

    callback when submitting job

--- lib/bushpig/client.rb
+++ lib/bushpig/client.rb
@@ -4,9 +4,10 @@ module Bushpig
   class Client
     attr_accessor :default_ttl
 
-    def initialize(pool)
+    def initialize(pool, &callback)
       @pool = pool
       @default_ttl = nil
+      @callback = callback
     end
 
     def redis_pool
@@ -18,6 +19,7 @@ module Bushpig
     end
 
     def submit(queue, job, score: default_score, ttl: default_ttl)
+      @callback.call(job)
       redis_pool.with do |conn|
         conn.set(Bushpig.job_key(job.job_id), job.job_payload, ex: ttl)
         conn.zadd(Bushpig.set_key(queue), score, job.job_id)
